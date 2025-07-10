c6a7e6ce4dd5748cad511a3dd7d0485fda4692ed
yaseen <yaseen.nvt@gmail.com>
2025-07-10 15:02:25 +0300

    upload: fix download building code button

--- buildcheck/components/employee_upload.py
+++ buildcheck/components/employee_upload.py
@@ -170,7 +170,16 @@ def footer() -> rx.Component:
         rx.spacer(),
         rx.text("Made by ARCH Authors", font_size="0.7em", color="gray"),
         rx.spacer(),
-        rx.button("📘 Building Guidelines", align_self="end", mt="1em", color_scheme="blue"),
+        rx.button(
+            rx.hstack(
+                rx.icon("download"),
+                rx.text("Building Guidelines", size="3"),
+            ),
+            align_self="end",
+            mt="1em",
+            color_scheme="blue",
+            on_click=rx.download(url="/sbc201.pdf")
+        ),
         padding="2em",
         spacing="5",
     )
