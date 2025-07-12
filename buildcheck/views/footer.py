911e64bd802df5b20508a68c5c5efa2f038c6a48
yaseen <yaseen.nvt@gmail.com>
2025-07-12 20:21:38 +0300

    footer: align with design

--- buildcheck/views/footer.py
+++ buildcheck/views/footer.py
@@ -2,23 +2,25 @@ import reflex as rx
 from buildcheck.components.bclogo import bclogo
 
 
-
 def footer() -> rx.Component:
-    return rx.hstack(
-    	bclogo(),
-        rx.spacer(),
-        rx.text("Made by ARCH Authors", font_size="0.7em", color="gray"),
-        rx.spacer(),
-        rx.button(
-            rx.hstack(
-                rx.icon("download"),
-                rx.text("Building Guidelines", size="3"),
+    return rx.box(
+        rx.vstack(
+            bclogo(),
+            rx.spacer(),
+            rx.text("© 2025  Inc. • Privacy • Terms "),
+            rx.spacer(),
+            rx.button(
+                rx.hstack(
+                    rx.icon("download"),
+                    rx.text("Building Guidelines", size="3"),
+                ),
+                mt="1em",
+                color_scheme="blue",
+                on_click=rx.download(url="/sbc201.pdf")
             ),
-            align_self="end",
-            mt="1em",
-            color_scheme="blue",
-            on_click=rx.download(url="/sbc201.pdf")
+            padding="2em",
+            spacing="3",
+            align="center"
         ),
-        padding="2em",
-        spacing="5",
+        width="100%"
     )
