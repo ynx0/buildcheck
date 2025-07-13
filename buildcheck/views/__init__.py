4f62ad6c72dcac931df6b4898f6e265612642c1e
jalsayid <jood.alsayid@gmail.com>
2025-07-13 09:40:41 +0300

    update login

--- buildcheck/views/__init__.py
+++ buildcheck/views/__init__.py
@@ -1,3 +1,4 @@
 from .validationView_reviewer import validation_page
+from .employeeBlueprint1 import employee_blueprint1
 
-__all__ = ["validation_page", ]
+__all__ = ["validation_page", "employee_blueprint1"]
