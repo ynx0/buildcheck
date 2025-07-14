a4449f4fd8249cbe3c0cf0908bf636d9784d0978
jalsayid <jood.alsayid@gmail.com>
2025-07-14 10:17:29 +0300

    adding footer

--- buildcheck/views/validationView_reviewer.py
+++ buildcheck/views/validationView_reviewer.py
@@ -1,6 +1,8 @@
 import reflex as rx
 from ..components.stats_cards import stats_cards_group
 from buildcheck.components.navbar import navbar
+from buildcheck.components.footer import footer
+
 
 def validation_page() -> rx.Component:
     my_child = rx.vstack(
@@ -12,5 +14,5 @@ def validation_page() -> rx.Component:
             width="100%",
             spacing="6",
             padding_x=["1.5em", "1.5em", "3em"],
-        )
+        ), footer(),
     return my_child
