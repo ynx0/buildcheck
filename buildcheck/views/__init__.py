d67d7d9242c16a9002d635a38967ff98e6cd9a5a
Noura-alnaimi <nouranasir33@gmail.com>
2025-07-15 10:11:34 +0300

    update notifications

--- buildcheck/views/__init__.py
+++ buildcheck/views/__init__.py
@@ -1,14 +1,9 @@
 from .validationView_reviewer import validation_page
-<<<<<<< Updated upstream
 from .employee_blueprint import employee_blueprint
-
-__all__ = ["validation_page", "employee_blueprint"]
-=======
-from .employeeBlueprint1 import employee_blueprint1
 from .employee_notfications import employee_notifications
 from .reviewer_notifications import reviewer_notifications
 from .admin_notifications import admin_notifications
 
 
-__all__ = ["validation_page", "employee_blueprint1", "employee_notifications", "reviewer_notifications", "admin_notifications"]
->>>>>>> Stashed changes
+__all__ = ["validation_page", "employee_blueprint", "employee_notifications", "reviewer_notifications", "admin_notifications"]
+
