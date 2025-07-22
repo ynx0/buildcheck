828254c7b61db98c9bc5087e7f1aae592d3ecfca
yaseen <yaseen.nvt@gmail.com>
2025-07-22 15:23:56 +0300

    backend: fix imports in blueprints

--- buildcheck/backend/blueprints.py
+++ buildcheck/backend/blueprints.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from Pillow import Image
+from PIL import Image
 import reflex as rx
 
 BLUEPRINT_HOME = Path('./uploaded_files')
@@ -8,7 +8,7 @@ BLUEPRINT_HOME = Path('./uploaded_files')
 
 
 def bp_name2path(file_name: str, employee_id: int):
-    user_dir = rx.get_upload_dir() / ('user_' + str(uid))
+    user_dir = rx.get_upload_dir() / ('user_' + str(employee_id))
     user_dir.mkdir(parents=True, exist_ok=True)
 
     return user_dir / file_name
