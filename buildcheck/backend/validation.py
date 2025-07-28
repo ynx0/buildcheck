c35ff5cef8fbab85610bff5a9ad8c500781485ba
yaseen <yaseen.nvt@gmail.com>
2025-07-28 11:28:44 +0300

    validation: initial integration

--- buildcheck/backend/validation.py
+++ buildcheck/backend/validation.py
@@ -2,20 +2,37 @@ from PIL import Image
 from .vectorization import *
 from pathlib import Path
 from .blueprints import *
+from .r2g_client import vectorize
+from .rule_engine import validate_ajyal
 
 
-def run_validation(image: Image) -> list[Failure]:
-    # TODO implement
+def run_validation(file_name: str, image: Image) -> list[Failure]:
     """
-    - get img from path
-    - encode image
+    Validation Pipeline
     - call r2g
-    - turn r2g into polygon
     - run yolo
     - run ocr
     - run rules
     """
-    return []
+
+    # call r2g on image
+    rooms = vectorize(image)
+    layout = Layout(rooms=rooms, file_name=file_name)
+
+    # run yolo on layout
+    processor_yolo = YOLOProcessor(image, YOLO_MODEL_PATH, layout)
+    processor_yolo.yoloProcesser()  # TODO see if we want 2.5
+    processor_yolo.print_room_summary()
+
+    # run OCR on the layout
+    processor_ocr = OCRProcessor(image, layout)
+    processor_ocr.ocrProcess()
+    processor_ocr.print_room_summary()
+
+    # run rules checking engine on layout
+    failures = validate_ajyal(layout)
+
+    return failures
 
 
 def run_validation_employee(file_name: str, employee_id: int) -> list[Failure]:
