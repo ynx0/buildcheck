fe19a5c5ad755570dd61f3f402be8490176a0ed1
yaseen <yaseen.nvt@gmail.com>
2025-07-29 19:13:31 +0300

    backend: create method bp_name2vispath and use in validation

--- buildcheck/backend/validation.py
+++ buildcheck/backend/validation.py
@@ -36,8 +36,7 @@ def run_validation(file_name: str, employee_id: int) -> list[Failure]:
 
     # run rules checking engine on layout
     failures = validate_ajyal(layout)
-    image_path = bp_name2path(file_name, employee_id)
-    output_path = image_path.with_name(image_path.stem + "_output.png")
+    image_path = bp_name2vispath(file_name, employee_id)
     visualizer = FloorPlanVisualizer(image_path, layout)
     visualizer.visualize(str(output_path))
 
