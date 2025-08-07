e5afbe99801e850f9f2bdd3d78f8ee7dea0ed7ef
yaseen <yaseen.nvt@gmail.com>
2025-08-07 11:28:57 +0300

    remove redundant file

--- employee_submitter_validation.sql
+++
@@ -1,8 +0,0 @@
-[?25l
-    Select a project:                                                                          
-                                                                                               
-  >  1. nivwovtmozmtsquqxtgy [name: ARCH-Project, org: jqlwymffnmeqjiubqtqd, region: us-east-1]
-                                                                                               
-                                                                                               
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                             
-                                                                                               [0D
